41b8022959fdd31fc007352b43f056d2d108cbd1
Andrew Bernier <a6bernier@gmail.com>
2018-09-03 02:17:58 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,8 @@ def loop_iterator(number_of_times)
   loop_iterator = 0
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"\
-    loop_iterator = 
+    loop_iterator += 1
+    
   end
 end
 
