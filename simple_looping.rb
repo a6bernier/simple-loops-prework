7bdd0c5ae8e433f7ee90c1ae1a659a7bbdf6be24
Andrew Bernier <a6bernier@gmail.com>
2018-09-03 02:32:40 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,7 +18,7 @@ end
 
 def while_iterator(number_of_times)
   while_iterator = 0
-  while while_iterator >= 7
+  while while_iterator <= 7
   while_iterator += 1
     puts "Welcome to Flatiron School's Web Development Course!"
   end
